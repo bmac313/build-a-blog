f47db342ad6a4d3d272b1f3f39b527a2b6a07a6a
Brendan <mcb0193@comcast.net>
2017-02-11 11:04:43 -0600

    Set up basic routes

--- main.py
+++ main.py
@@ -18,14 +18,46 @@ import webapp2
 import cgi
 import jinja2
 import os
+from google.appengine.ext import db
+
+
+class Post(db.Model):
+	title = db.StringProperty(required=True)
+	body = db.TextProperty(required=True)
+	publish_time = db.DateTimeProperty(auto_now_add=True)
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-class MainHandler(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+	"""Base request handler class for the entire app"""
+	def renderError(self, error_code):
+		self.error(error_code)
+		self.response.write("Something went wrong while loading this page! If you'd like more info on the error, you can search for the error code above.")
+
+class MainHandler(Handler):
 	def get(self):
-		self.response.write('Hello world!')
+		self.redirect('/blog')
+		
+class MainBlog(Handler):
+	def get(self):
+		t = jinja_env.get_template("main-blog.html")
+		page_content = t.render(post_title = "Test", post_body = "lorem ipsum text")
+		self.response.write(page_content)
+		
+class NewPost(Handler):
+	def get(self):
+		t = jinja_env.get_template("new-post.html")
+		page_content = t.render()
+		self.response.write(page_content)
+
+class ViewPost(Handler):
+	def get(self):
+		pass
 
 app = webapp2.WSGIApplication([
-	('/', MainHandler)
+	('/', MainHandler),
+	('/blog', MainBlog),
+	('/newpost', NewPost),
+	webapp2.Route('/post/<post_id:\d+>', ViewPost)
 ], debug=True)
